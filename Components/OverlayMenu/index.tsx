69d182dbee1606e47fe17522891e45321596d559
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-03 15:58:06 +0330

    fix: add slide animation to menu

--- Components/OverlayMenu/index.tsx
+++ Components/OverlayMenu/index.tsx
@@ -1,9 +1,19 @@
 
 import { MenuContext } from "@/context/MenuProvider"
+import classNames from 'classnames';
+import { useContext } from "react";
 
 const OverlayMenu = () => {
+
+  let menuVisibilty = useContext(MenuContext)
+
+  const OverlayClasses = classNames('w-full h-56 px-4 transition-all relative top-0 z-10',{
+    'left-0': menuVisibilty.menu,
+    '-left-80': !menuVisibilty.menu,
+
+})
   return (
-    <section className="w-full h-56 px-4 transition-all">
+    <section className={OverlayClasses}>
       <div className="w-full h-full bg-white rounded-xl">
           
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 "use client"
 
-import OverlayMenu from '@/Components/OverlayMenu'
-import { MenuContext } from "@/context/MenuProvider";
-import { useContext } from 'react';
+import OverlayMenu from "@/Components/OverlayMenu"
 
 export default function Home() {
 
-  let menuVisibility = useContext(MenuContext)
-
   return (
     <>
-      {
-        menuVisibility.menu && <OverlayMenu />
-      }
+    <OverlayMenu/>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       </main>
